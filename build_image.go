f59f027e49cd7dd9cceb3edec1a02aba68e2bc6f
shenshengkafei <wudiadi1111@gmail.com>
2017-11-07 14:33:55 -0800

    Added push image to build_image

--- build_image.go
+++ build_image.go
@@ -1,67 +1,83 @@
 package main
 
 import (
-    "archive/tar"
-    "bytes"
-    "context"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
+	"archive/tar"
+	"bytes"
+	"context"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 
-    "github.com/docker/docker/api/types"
-    "github.com/docker/docker/client"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-    ctx := context.Background()
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        log.Fatal(err, " :unable to init client")
-    }
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		log.Fatal(err, " :unable to init client")
+	}
 
-    buf := new(bytes.Buffer)
-    tw := tar.NewWriter(buf)
-    defer tw.Close()
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
 
-    dockerFile := "myDockerfile"
-    dockerFileReader, err := os.Open("/home/shenshengkafei/swagger/src/IO.Swagger/Dockerfile")
-    if err != nil {
-        log.Fatal(err, " :unable to open Dockerfile")
-    }
-    readDockerFile, err := ioutil.ReadAll(dockerFileReader)
-    if err != nil {
-        log.Fatal(err, " :unable to read dockerfile")
-    }
+	dockerFile := "myDockerfile"
+	dockerFileReader, err := os.Open("/home/shenshengkafei/swagger/src/IO.Swagger/Dockerfile")
+	if err != nil {
+		log.Fatal(err, " :unable to open Dockerfile")
+	}
+	readDockerFile, err := ioutil.ReadAll(dockerFileReader)
+	if err != nil {
+		log.Fatal(err, " :unable to read dockerfile")
+	}
 
-    tarHeader := &tar.Header{
-        Name: dockerFile,
-        Size: int64(len(readDockerFile)),
-    }
-    err = tw.WriteHeader(tarHeader)
-    if err != nil {
-        log.Fatal(err, " :unable to write tar header")
-    }
-    _, err = tw.Write(readDockerFile)
-    if err != nil {
-        log.Fatal(err, " :unable to write tar body")
-    }
-    dockerFileTarReader := bytes.NewReader(buf.Bytes())
+	tarHeader := &tar.Header{
+		Name: dockerFile,
+		Size: int64(len(readDockerFile)),
+	}
+	err = tw.WriteHeader(tarHeader)
+	if err != nil {
+		log.Fatal(err, " :unable to write tar header")
+	}
+	_, err = tw.Write(readDockerFile)
+	if err != nil {
+		log.Fatal(err, " :unable to write tar body")
+	}
+	dockerFileTarReader := bytes.NewReader(buf.Bytes())
 
-    imageBuildResponse, err := cli.ImageBuild(
-        ctx,
-        dockerFileTarReader,
-        types.ImageBuildOptions{
-            Tags:   []string{"shenshengkafei/imagename"},
-            Context:    dockerFileTarReader,
-            Dockerfile: dockerFile,
-            Remove:     true})
-    if err != nil {
-        log.Fatal(err, " :unable to build docker image")
-    }
-    defer imageBuildResponse.Body.Close()
-    _, err = io.Copy(os.Stdout, imageBuildResponse.Body)
-    if err != nil {
-        log.Fatal(err, " :unable to read image build response")
-    }
+	imageBuildResponse, err := cli.ImageBuild(
+		ctx,
+		dockerFileTarReader,
+		types.ImageBuildOptions{
+			Tags:       []string{"shenshengkafei/imagename"},
+			Context:    dockerFileTarReader,
+			Dockerfile: dockerFile,
+			Remove:     true})
+	if err != nil {
+		log.Fatal(err, " :unable to build docker image")
+	}
+	defer imageBuildResponse.Body.Close()
+	_, err = io.Copy(os.Stdout, imageBuildResponse.Body)
+	if err != nil {
+		log.Fatal(err, " :unable to read image build response")
+	}
+
+	imagePushResponse, err := cli.ImagePush(
+		context.Background(),
+		"shenshengkafei/imagename",
+		types.ImagePushOptions{
+			All:          true,
+			RegistryAuth: "123",
+		})
+	if err != nil {
+		log.Fatal(err, " :unable to push docker image")
+	}
+	defer imagePushResponse.Close()
+	_, err = io.Copy(os.Stdout, imagePushResponse)
+	if err != nil {
+		log.Fatal(err, " :unable to read image push response")
+	}
 }
